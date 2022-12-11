65e1acef32679b30243abdcfa2929bf37d205239
Simon Atiegar <atiegardelali@gmail.com>
2022-12-11 22:54:01 +0000

    update scores

--- Quiz_game.py
+++ Quiz_game.py
@@ -1,12 +1,19 @@
-print("Welcom to my Quiz Game!")
-
+print("Welcom to my Quiz Game!") 
 play = input("Do you want to play? ")
 
-if play != "yes":
+if play.lower() != "yes":
     quit()
 else:
     print("Okay! Let's do this")
+score = 0
 
 answer = input("what does CPU stand for?\n")
-if answer == "central processing unit" or "Central Processing Unit":
+if answer.lower() == "central processing unit":
     print('Correct!')
+    score += 1
+else:
+    print('Incorrect')
+# You can add more quiz below
+
+# Printing your scores
+print(f"You got {score} points")
